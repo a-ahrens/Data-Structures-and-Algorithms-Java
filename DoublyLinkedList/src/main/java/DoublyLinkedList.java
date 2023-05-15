46daf2d67ee0db9b7c3a0b2a54a82b1dd68226ea
Anthony Ahrens <anthony.l.ahrens@gmail.com>
2023-05-15 14:48:11 -0500

    Added new methods for append, prepend, insert, set, get, removeFirst, removeLast

--- DoublyLinkedList/src/main/java/DoublyLinkedList.java
+++ DoublyLinkedList/src/main/java/DoublyLinkedList.java
@@ -23,9 +23,10 @@ public class DoublyLinkedList {
     public void printList() {
         Node temp = head;
         while(temp != null) {
-            System.out.println(temp.value);
+            System.out.print(temp.value + " , ");
             temp = temp.next;
         }
+        System.out.println();
     }
 
     public Node getHead() {
@@ -39,4 +40,139 @@ public class DoublyLinkedList {
     public int getLength() {
         return length;
     }
+
+    //add new node to the end of list
+    public void append(int value){
+        Node newNode = new Node(value);
+
+        if(length == 0){
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+
+        length++;
+    }
+
+    public Node removeLast(){
+        /*check to see if the list is empty prior to doing anything
+            return null if so */
+        if(length == 0){
+            return null;
+        }
+
+        Node temp = tail;       //create a marker pointing at current tail
+
+        if(length == 1){
+            head = null;
+            tail = null;
+        } else {
+            tail = tail.prev;       //move the tail marker to the previous node
+            tail.next = null;       //break the new tail location from the old tail location (currently the temp node)
+            temp.prev = null;       //break the temp node from the new tail node
+        }
+
+        length--;
+        return temp;
+    }
+
+    //add a new node to the front of the list
+    public void prepend(int value){
+        Node newNode = new Node(value);
+
+        if(length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        length++;
+    }
+
+    //remove first node from list
+    public Node removeFirst(){
+
+        if(length == 0){
+            return null;
+        }
+
+        Node temp = head;
+        if(length == 1){
+            head = null;
+            tail = null;
+
+        } else {
+            head = head.next;
+            head.prev = null;
+            temp.next = null;
+        }
+        length--;
+
+        return temp;
+    }
+
+    public Node get(int index){
+        if(index < 0 || index >= length){
+            return null;
+        }
+
+        Node temp = head;
+        if(index <= length / 2){
+            for(int i = 0; i < index; i++ ){
+                temp = temp.next;
+            }
+        } else {
+            temp = tail;
+            for(int i = length - 1; i > index; i--){
+                temp = temp.prev;
+            }
+        }
+
+        return temp;
+    }
+
+    public boolean set(int value, int index){
+        Node temp = get(index);
+        if(temp == null) {
+            return false;
+        }
+
+        temp.value = value;
+        return true;
+    }
+
+    public boolean insert(int value, int index){
+        Node newNode = new Node(value);
+
+        if(index < 0 || index > length){
+            return false;
+        }
+
+        if(index == 0){
+            prepend(value);
+            return true;
+        }
+
+        if(index == length){
+            append(value);
+            return true;
+        }
+
+        Node before = get(index - 1);
+        Node after = before.next;
+
+        newNode.prev = before;
+        newNode.next = after;
+        before.next = newNode;
+        after.prev = newNode;
+        length++;
+
+        return true;
+    }
+
 }
--- DoublyLinkedList/src/main/java/Main.java
+++ DoublyLinkedList/src/main/java/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args){
 
-        DoublyLinkedList myDLL = new DoublyLinkedList(7);
-
+        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+        myDLL.append(2);
+        myDLL.prepend(3);
+        myDLL.insert(11,2);
         myDLL.printList();
+
+        System.out.println("Get node at 0: " + myDLL.get(0).value);
+        System.out.println("Get node at 1: " + myDLL.get(1).value);
+
     }
 }
