cebd8846b3f17984b3c68e12a0af5201773a3711
Anthony Ahrens <anthony.l.ahrens@gmail.com>
2023-06-01 15:14:21 -0500

    Added Selection Sort algo

--- Algorithms/Sorting-Algorithms/src/main/java/Main.java
+++ Algorithms/Sorting-Algorithms/src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int[] array = {4,2,6,5,1,3};
         System.out.println("Not sorted: " + Arrays.toString(array));
-        bubbleSort(array);
+        selectionSort(array);
         System.out.println("Sorted: " + Arrays.toString(array));
     }
 
@@ -16,6 +16,22 @@ public class Main {
                     array[j] = array[j+1];
                     array[j+1] = temp;
                 }
+            }
+        }
+    }
+
+    public static void selectionSort(int[] array){
+        for(int i = 0; i < array.length; i++){
+            int minIndex = i;
+            for(int j = i + 1; j < array.length; j++){
+                if(array[minIndex] > array[j]){
+                    minIndex = j;
+                }
+            }
+            if(minIndex != i){
+                int temp = array[minIndex];
+                array[minIndex] = array[i];
+                array[i] = temp;
             }
         }
     }
