92f5cf5064ca1d21010f27933156f3c2a34c32cb
Anthony Ahrens <anthony.l.ahrens@gmail.com>
2023-06-13 13:32:00 -0500

    Completed study on Queue data structures.

--- Queues/src/main/java/Main.java
+++ Queues/src/main/java/Main.java
@@ -1,2 +1,27 @@
 public class Main {
+
+    public static void main(String[] args) {
+        Queue myQueue = new Queue(3);
+
+        myQueue.getFirst();
+        myQueue.getLast();
+        myQueue.getLength();
+        myQueue.printQueue();
+        System.out.println();
+
+        myQueue.enqueue(7);
+
+        myQueue.getFirst();
+        myQueue.getLast();
+        myQueue.getLength();
+        myQueue.printQueue();
+
+
+        System.out.println(myQueue.dequeue().value);
+        System.out.println(myQueue.dequeue().value);
+        System.out.println(myQueue.dequeue());
+
+
+
+    }
 }
